0ae432964bad378c6b94aef51483da4e8c3d0adb
goShell <herkodandy@gmail.com>
2025-06-01 23:48:30 +0300

    fix: update test_client.py:114:80: E501 line too long (113 > 79 characters)

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -111,7 +111,8 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         def get_response(url, *args, **kwargs):
             expected_org_url = "https://api.github.com/orgs/google"
             if url == expected_org_url:
-                return MockResponse({"login": "google", "repos_url": "https://api.github.com/orgs/google/repos"})
+                return MockResponse({"login": "google",
+                        "repos_url": "https://api.github.com/orgs/google/repos"})
             elif url == "https://api.github.com/orgs/google/repos":
                 return MockResponse(cls.repos_payload)
             return MockResponse(None)
