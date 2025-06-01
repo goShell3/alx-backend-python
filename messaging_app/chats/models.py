2a3a8627cc0a78d4d2fed79e91633310c0b807f0
goShell <herkodandy@gmail.com>
2025-06-01 22:32:01 +0300

    updates: implemented sent_at filed inside the Message model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -109,7 +109,8 @@ class Message(models.Model):
         on_delete=models.CASCADE
     )
     message_body = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.sender.username}: {self.message_body[:30]}..."
