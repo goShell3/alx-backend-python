a57ba9c924fd47f6b5dc2b1cf4a002afa79f9d8c
goShell <herkodandy@gmail.com>
2025-06-08 12:34:40 +0300

    feat: add usercanreadonly basepermission for the UserCanViewMessage auth poliy

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,12 +1,21 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
+
+class UserCanReadonly(BasePermission);
+    def has_permission(self, request, view):
+        return request.has_permission(request, view)
+    
 class UserCanViewMessage(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated|UserCanReadonly]
 
     def get(self, request, format=None):
         content = {
             'status': 'request was permitted'
         }
         return Response(content)
+    
