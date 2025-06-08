c236b4e2a30df11a07a2db312a946714f7f0b0a8
goShell <herkodandy@gmail.com>
2025-06-08 12:31:45 +0300

    feat: add the UserCanViewMessage permission set for each users

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class UserCanViewMessage(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
