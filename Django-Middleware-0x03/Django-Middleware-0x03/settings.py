2326f4c56b660300be478a3f386302e65a323369
goShell <herkodandy@gmail.com>
2025-06-08 18:26:05 +0300

    mvd: created 0x03-MessagingApp-Djanog app

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'chats.middleware.RestrictAccessByTimeMiddleware',
     'chats.middleware.RateLimitMiddleware',
     'chats.middleware.OffensiveLanguageMiddleware',
-    'chats.middleware.RolePermissionMiddleware',
+    "chats.middleware.RolepermissionMiddleware"
 ]  
 
 ROOT_URLCONF = 'messaging_app.urls'
